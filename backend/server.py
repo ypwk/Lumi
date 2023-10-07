0c13cb39fb540bcd71089fe6506bdb10f9a013a3
Kevin Wu <28658818+ypwk@users.noreply.github.com>
2023-10-07 03:33:05 -0400

    backend update

--- backend/server.py
+++ backend/server.py
@@ -1,14 +1,26 @@
 #! /usr/bin/python3
 from flask import Flask, request, jsonify
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from optimum.onnxruntime import ORTModelForQuestionAnswering
+from transformers import AutoTokenizer, pipeline, AutoModelForCausalLM
 from langchain.llms import HuggingFacePipeline
 from langchain.prompts import PromptTemplate
+import time
 
-llm = HuggingFacePipeline.from_model_id(
-    model_id="garage-bAInd/Platypus2-7B",
-    task="text-generation",
-    model_kwargs={"temperature": 0, "max_length": 64},
-)
+# model_id = "garage-bAInd/Platypus2-7B"
+model_id = "mistralai/Mistral-7B-v0.1"
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+model = AutoModelForCausalLM.from_pretrained(model_id)
+pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, max_new_tokens=30)
+
+tokenizer.pad_token_id = model.config.eos_token_id
+
+llm = HuggingFacePipeline(pipeline=pipe)
+
+template = """Question: {question}
+
+Answer: Let's think step by step."""
+prompt = PromptTemplate.from_template(template)
+chain = prompt | llm
 
 app = Flask(__name__)
 
@@ -17,8 +29,13 @@ def hello_world():
     return "This is my first API sadf!"
 
 
-@app.route("/post", methods=["POST"])
-def testpost():
-    input_json = request.get_json(force=True)
-    dictToReturn = {"text": input_json["text"]}
+@app.route("/query/<question>", methods=["GET"])
+def query(question):
+    start = time.time()
+    response = chain.invoke({"question": question})
+    end = time.time()
+    dictToReturn = {
+        "response": response,
+        "time_to_compute": str(end - start)
+    }
     return jsonify(dictToReturn)
